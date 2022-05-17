4ac61775a85340f86d8ff83458784476f1101185
tangusjr <kipngenogilbert96@gmail.com>
2022-05-17 19:49:03 +0300

    created variable and functions for arrayof Hospoitals

--- assets/js/hospital.js
+++ assets/js/hospital.js
@@ -1,16 +1,127 @@
+const hospData = [
+  {
+    hospName: "Karen Hospital",
+    location: "Nairobi",
+    consultationFee: 2000,
+    specialty: "Heart Conditions, Cancer",
+  },
+  {
+    hospName: "Agha Khan Hospital",
+    location: "Nairobi",
+    consultationFee: 3000,
+    specialty: "Cancer, Maternal Health",
+  },
+  {
+    hospName: "Kenyatta National Hospital",
+    location: "Nairobi",
+    consultationFee: 2000,
+    specialty: "Pain Mananagement, Cancer",
+  },
+  {
+    hospName: "Nairobi Hospital",
+    location: "Nairobi",
+    consultationFee: 2500,
+    specialty: "Reproductive Health, Diabetes",
+  },
+  {
+    hospName: "Nairobi West Hospital",
+    location: "Nairobi",
+    consultationFee: 2500,
+    specialty: "Peadiatric health",
+  },
+  {
+    hospName: "M.P Shah Hospital",
+    location: "Nairobi",
+    consultationFee: 3000,
+    specialty: "Heart Conditions, Cancer",
+  },
+];
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
 
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
 
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
 
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
 
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
 
-}
+//constnats for first hospital
+const hospNameOne = document.getElementById("li-1");
+const locationName = document.getElementById("li-2");
+const feeOne = document.getElementById("li-3");
+const specialtyOne = document.getElementById("li-4");
 
-
-
-
-]
+//function for the first doctor
+const getHospOne = () => {
+  hospNameOne.innerHTML = hospData[0].hospName;
+  locationName.innerHTML = hospData[0].location;
+  feeOne.innerHTML = hospData[0].consultationFee;
+  specialtyOne = hospData[0].specialty;
+};
+getHospOne();
